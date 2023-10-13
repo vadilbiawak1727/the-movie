0a1ff5a681275737a3289a6813945d20c07bbdfe
vadilbiawak1727 <104118998+vadilbiawak1727@users.noreply.github.com>
2023-10-13 14:42:29 +0700

    Update http.dart
    update

--- lib/http.dart
+++ lib/http.dart
@@ -1,35 +1,120 @@
 import 'package:flutter/material.dart';
-import 'package:the_movie/HttpHelper.dart';
 
-class MyHome extends StatefulWidget {
-  const MyHome({super.key});
+import 'httpHelper.dart';
+
+class Screen1 extends StatefulWidget {
+  const Screen1({Key? key}) : super(key: key);
 
   @override
-  State<MyHome> createState() => _MyHomeState();
+  State<Screen1> createState() => _Screen1State();
 }
 
-class _MyHomeState extends State<MyHome> {
+class _Screen1State extends State<Screen1> {
   late String result;
   late HttpHelper helper;
+  String selectedCategory = 'now_playing';
 
-@override
-void initState() {
-  super.initState();
-  helper = HttpHelper();
-  result = "";
-}
+  @override
+  void initState() {
+    super.initState();
+    helper = HttpHelper();
+    result = "";
+
+    fetchMovies(selectedCategory);
+  }
+
+  Future<void> fetchMovies(String category) async {
+    final String movieData = await helper.getMoviesByCategory(category);
+    setState(() {
+      result = movieData;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    helper.getMovie().then((value) {
-      setState(() {
-        result = value;
-      });
-    });
     return Scaffold(
-      appBar: AppBar(title: Text("My Video")),
-      body: SingleChildScrollView(
-        child: Text("$result"),
+      appBar: AppBar(
+        title: Text('My Movie'),
+        backgroundColor: Color.fromARGB(255, 46, 217, 255),
+      ),
+      body: Column(
+        children: <Widget>[
+          // Ubah dropdown menu menjadi radio button
+          Column(
+            children: [
+              RadioListTile<String>(
+                value: 'latest',
+                title: Text('Latest'),
+                groupValue: selectedCategory,
+                onChanged: (String? newValue) {
+                  if (newValue != null) {
+                    setState(() {
+                      selectedCategory = newValue;
+                    });
+                    fetchMovies(newValue);
+                  }
+                },
+              ),
+              RadioListTile<String>(
+                value: 'now_playing',
+                title: Text('Now Playing'),
+                groupValue: selectedCategory,
+                onChanged: (String? newValue) {
+                  if (newValue != null) {
+                    setState(() {
+                      selectedCategory = newValue;
+                    });
+                    fetchMovies(newValue);
+                  }
+                },
+              ),
+              RadioListTile<String>(
+                value: 'popular',
+                title: Text('Popular'),
+                groupValue: selectedCategory,
+                onChanged: (String? newValue) {
+                  if (newValue != null) {
+                    setState(() {
+                      selectedCategory = newValue;
+                    });
+                    fetchMovies(newValue);
+                  }
+                },
+              ),
+              RadioListTile<String>(
+                value: 'top_rated',
+                title: Text('Top Rated'),
+                groupValue: selectedCategory,
+                onChanged: (String? newValue) {
+                  if (newValue != null) {
+                    setState(() {
+                      selectedCategory = newValue;
+                    });
+                    fetchMovies(newValue);
+                  }
+                },
+              ),
+              RadioListTile<String>(
+                value: 'upcoming',
+                title: Text('Upcoming'),
+                groupValue: selectedCategory,
+                onChanged: (String? newValue) {
+                  if (newValue != null) {
+                    setState(() {
+                      selectedCategory = newValue;
+                    });
+                    fetchMovies(newValue);
+                  }
+                },
+              ),
+            ],
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Text("$result"),
+            ),
+          ),
+        ],
       ),
     );
   }
